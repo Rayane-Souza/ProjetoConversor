b13e4bd5e2635a0313a365c646e0d8a7458c92a2
Rayane <rayannesilvasouza@outlook.com>
2023-12-20 13:35:53 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
     <ConversorComponent moeda-a="USD" moeda-b="BRL"/>
+    <ConversorComponent moeda-a="BRL" moeda-b="USD"/>
+</div>
+<div class="linha">
+    <ConversorComponent moeda-a="CAD" moeda-b="BRL"/>
+    <ConversorComponent moeda-a="BRL" moeda-b="CAD"/>
+</div>
+<div class="linha">
+    <ConversorComponent moeda-a="EUR" moeda-b="BRL"/>
+    <ConversorComponent moeda-a="BRL" moeda-b="EUR"/>
+</div>
+
+
   </div>
 </template>
 
@@ -14,18 +28,23 @@ export default {
   } 
 }
 
-
-
-
 </script>
 
 <style>
 #app {
+  margin: auto;
+  max-width: 900px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.linha {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+
 }
 </style>
--- src/components/ConversorComponent.vue
+++ src/components/ConversorComponent.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="conversorcomponent">
+    <div class="conversorcomponent">
 <h2>{{moedaA}} Para {{moedaB}} </h2>
 <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-<input type="button" value="Converter">
+<input type="button" value="Converter" v-on:click="converter">
 <h2>{{ moedaB_value}}</h2>
 
 
-</div>
+</div> 
 
 </template>
 
@@ -26,11 +26,30 @@ export default {
 
         
         converter(){
+            let de_para = this.moedaA + "_" + this.moedaB;
+
+
+            let url = 
+            "https://free.currconv.com/api/v7/convert?q="+
+            de_para +
+            "&compact=ultra&apiKey=7d4974bd1fe3479269f7";
+
+            console.log('URL:', url);
+
+
+
+            fetch(url)
+            .then(res => {
+                return res.json();
+            })
+
+            .then(json => { 
+                let cotacao = json[de_para];
+                this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2)
+
+            })
 
         }
-
-
-
 
     }
    
@@ -41,6 +60,13 @@ export default {
 </script>
 
 <style scoped>
+
+.conversorcomponent{
+
+    padding: 20px;
+    max-width: 300px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
 
 
 </style>
